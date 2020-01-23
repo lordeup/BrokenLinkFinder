5558b8c8d1a58c3ed3206ba6c2185cae0e52dedc
Igor Taigin <itaigin@plotpad.ru>
2020-01-23 12:41:07 +0300

    added thread number

--- src/main/java/com/files/property/Property.java
+++ src/main/java/com/files/property/Property.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Property {
-    private Properties properties = new Properties();
+    private Properties properties;
 
     public Property() throws IOException {
+        properties = new Properties();
         String pathToProperties = "src/main/resources/config.properties";
-        FileInputStream fileInputStream = new FileInputStream(pathToProperties);
-        properties.load(fileInputStream);
+        try (FileInputStream fileInputStream = new FileInputStream(pathToProperties)) {
+            properties.load(fileInputStream);
+        }
     }
 
     public int getConnectionTimeout() {
@@ -18,6 +20,14 @@ public class Property {
             return Integer.parseInt(properties.getProperty("connectionTimeout"));
         } catch (NumberFormatException e) {
             return 10000;
+        }
+    }
+
+    public int getThreadNumber() {
+        try {
+            return Integer.parseInt(properties.getProperty("threadNumber"));
+        } catch (NumberFormatException e) {
+            return 0;
         }
     }
 }
